e00b94f88c455cd9894d913196f73f355de15e6c
Luis G. Jaramillo <lgjaramillo@gmail.com>
2023-11-22 22:16:54 -0600

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,65 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import * as jsPlumb from "@jsplumb/browser-ui";
-require("@jsplumb/browser-ui");
-
-const node1 = {
-  id: "node1",
-  label: "Node 1",
-  x: 100,
-  y: 100,
-};
-
-const node2 = {
-  id: "node2",
-  label: "Node 2",
-  x: 200,
-  y: 200,
-};
+import * as jsPlumbBrowserUI from "@jsplumb/browser-ui";
 
 function App() {
-  const [plumb, setPlumb] = useState(null);
 
   useEffect(() => {
-    const container = document.getElementById("jsplumb-container");
-    if (container) {
-      const instance = jsPlumb.newInstance({
-        container: container,
-      });
-      console.log(instance);
-      setPlumb(instance.BrowserJsPlumbInstance);
-    }
-  }, []);
+    const element = document.getElementById("jsplumb-container");
+    const instance = jsPlumbBrowserUI.newInstance({
+      container: element,
+    });
+    const start = document.getElementById("start");
+    const node1 = document.getElementById("node1");
+    const node2 = document.getElementById("node2");
 
-  useEffect(() => {
-    /*
-    if (plumb) {
-      plumb.addEndpoint("node1", {
-        anchor: "Right",
-        maxConnections: -1,
-      });
-      plumb.addEndpoint("node2", {
-        anchor: "Left",
-        maxConnections: -1,
-      });
-      plumb.connect({
-        source: "node1",
-        target: "node2",
-      });
-    }
-    */
+    instance.manageAll([ start, node1, node2]);
+
+    instance.connect({
+      source: start,
+      target: node1,
+      anchors: ["Right", "Left"],
+      connector:{ 
+        type:"Bezier",
+        options:{
+          curviness: 50,
+        },
+      },
   });
+
+    instance.connect({
+      source: node1,
+      target: node2,
+      connector:{
+        type: "Flowchart",
+        options:{
+          stub: [10, 10],
+          // gap: 10,
+          cornerRadius: 10,
+        },
+      },
+      anchor:"Continuous",
+      /*
+      overlays:[
+        { type:"Label", options:{label:"Connection 2", location: 0.5, cssClass:"myLabel"}},
+        { type:"Arrow", options:{location:1}},
+      ]
+      */
+    });
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div
-        id="jsplumb-container"
-        style={{ width: "100%", height: "100%", position: "relative" }}
-      >
+      <header className="App-header">
         <div
-          id="node1"
-          style={{
-            position: "absolute",
-            left: node1.x,
-            top: node1.y,
-            width: 100,
-            height: 100,
-            backgroundColor: "red",
-          }}
+          id="jsplumb-container"
+          style={{ width: "1000px", height: "600px", position: "relative", border: "1px solid black" }}
         >
-          {node1.label}
+        <div id="start" className="node" style={{ left: 100, top: 200 }}>Inicio</div>
+        <div id="node1" className="node" style={{ left: 300, top: 300 }}>Fase 1</div>
+        <div id="node2" className="node" style={{ left: 600, top: 400 }}>Fase 2</div>
         </div>
-        <div
-          id="node2"
-          style={{
-            position: "absolute",
-            left: node2.x,
-            top: node2.y,
-            width: 100,
-            height: 100,
-            backgroundColor: "blue",
-          }}
-        >
-          {node2.label}
-        </div>
-      </div>
+      </header>
     </div>
   );
 }
