2f1228a86732c1a52a0a21592aabc11ce16c2c9c
Linuxsapiens <lgjaramillo@gmail.com>
2023-11-23 17:03:16 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import * as jsPlumbBrowserUI from "@jsplumb/browser-ui";
 
 function App() {
-
   useEffect(() => {
     const element = document.getElementById("jsplumb-container");
     const instance = jsPlumbBrowserUI.newInstance({
@@ -13,32 +12,32 @@ function App() {
     const node1 = document.getElementById("node1");
     const node2 = document.getElementById("node2");
 
-    instance.manageAll([ start, node1, node2]);
+    instance.manageAll([start, node1, node2, node3]);
 
     instance.connect({
       source: start,
       target: node1,
       anchors: ["Right", "Left"],
-      connector:{ 
-        type:"Bezier",
-        options:{
+      connector: {
+        type: "Bezier",
+        options: {
           curviness: 50,
         },
       },
-  });
+    });
 
     instance.connect({
       source: node1,
       target: node2,
-      connector:{
+      anchors: ["Right", "Left"],
+      connector: {
         type: "Flowchart",
-        options:{
+        options: {
           stub: [10, 10],
           // gap: 10,
           cornerRadius: 10,
         },
       },
-      anchor:"Continuous",
       /*
       overlays:[
         { type:"Label", options:{label:"Connection 2", location: 0.5, cssClass:"myLabel"}},
@@ -53,11 +52,25 @@ function App() {
       <header className="App-header">
         <div
           id="jsplumb-container"
-          style={{ width: "1000px", height: "600px", position: "relative", border: "1px solid black" }}
+          style={{
+            width: "1000px",
+            height: "600px",
+            position: "relative",
+            border: "1px solid black",
+          }}
         >
-        <div id="start" className="node" style={{ left: 100, top: 200 }}>Inicio</div>
-        <div id="node1" className="node" style={{ left: 300, top: 300 }}>Fase 1</div>
-        <div id="node2" className="node" style={{ left: 600, top: 400 }}>Fase 2</div>
+          <div id="start" className="node" style={{ left: 100, top: 200 }}>
+            Inicio
+          </div>
+          <div id="node1" className="node" style={{ left: 300, top: 300 }}>
+            Fase 1
+          </div>
+          <div id="node2" className="node" style={{ left: 600, top: 400 }}>
+            Fase 2
+          </div>
+          <div id="node3" className="node" style={{ left: 900, top: 500 }}>
+            Fase 3
+          </div>
         </div>
       </header>
     </div>
